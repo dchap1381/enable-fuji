9368502626ae44bf437be88eb08ac36ff7424605
Davis Chappell <64147519+dchap1381@users.noreply.github.com>
2022-07-01 12:05:45 -0400

    Update installfuji.sh

--- installfuji.sh
+++ installfuji.sh
@@ -3,7 +3,7 @@
 
 lpadmin -p Fujitsu -E -v usb://FIT/FP-2000?serial=0 -m CBM1000.ppd
 
-# Create script to enable Fuji printer
+#Create script to enable Fuji printer
 
 echo "#!/bin/bash" > /home/pi/enable-fujitsu.sh
 echo "lpadmin -d Fujitsu" >> /home/pi/enable-fujitsu.sh
@@ -14,3 +14,14 @@ chmod +x /home/pi/enable-fujitsu.sh
 echo "#!/bin/bash" > /home/pi/enable-epson.sh
 echo "lpadmin -d EpsonT88" >> /home/pi/enable-epson.sh
 chmod +x /home/pi/enable-epson.sh
+
+### added 12-10-21 by dcc ###
+# Enable Citizen CT-S310II printer on USB using CT-S310II specific driver
+
+lpadmin -p Citizen -E -v usb://CITIZEN/CT-S310II?serial=00000000 -m CTS310II.ppd
+
+# Create script to enable Citizen CT-S310II
+
+echo "#!/bin/bash" > /home/pi/enable-citizen.sh
+echo "lpadmin -d Citizen" >> /home/pi/enable-citizen.sh
+chmod +x /home/pi/enable-citizen.sh
