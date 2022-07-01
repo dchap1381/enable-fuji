736068fcb0b46557db2c17159a63455fdc5fce62
Davis Chappell <64147519+dchap1381@users.noreply.github.com>
2022-07-01 12:02:14 -0400

    Update installfuji.sh

--- installfuji.sh
+++ installfuji.sh
@@ -5,12 +5,12 @@ lpadmin -p Fujitsu -E -v usb://FIT/FP-2000?serial=0 -m CBM1000.ppd
 
 # Create script to enable Fuji printer
 
-echo "#!/bin/bash" >> /home/pi/enable-fujitsu.sh
+echo "#!/bin/bash" > /home/pi/enable-fujitsu.sh
 echo "lpadmin -d Fujitsu" >> /home/pi/enable-fujitsu.sh
 chmod +x /home/pi/enable-fujitsu.sh
 
 # Create script to enable Epson printer
 
-echo "#!/bin/bash" >> /home/pi/enable-epson.sh
+echo "#!/bin/bash" > /home/pi/enable-epson.sh
 echo "lpadmin -d EpsonT88" >> /home/pi/enable-epson.sh
 chmod +x /home/pi/enable-epson.sh
